331f37ea7ce0b40b767ff1b0f05d9ee38d031e71
jinwooPark6 <jinwoo13245@naver.com>
2021-03-17 02:28:43 +0900

    survlet Life-Cycle

--- src/com/servlet/TestServlet.java
+++ src/com/servlet/TestServlet.java
@@ -1,7 +1,6 @@
 package com.servlet;
 
 import java.io.IOException;
-import java.io.PrintWriter;
 
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
@@ -12,37 +11,36 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 
-@WebServlet("/SE1")
-public class ServletEx extends HttpServlet {
-   
-	// servlet 실행
+@WebServlet("/ts")
+public class TestServlet extends HttpServlet {
+ 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.getWriter().append("Served at: ").append(request.getContextPath());
 		System.out.println("-- doGet() --");
 	}
-	
+
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		doGet(request, response);
 	}
-
+	
 	@PostConstruct
-	public void postConstruct() {
-		System.out.println("--- postConstruct() ---");
+	public void funPc() {
+		System.out.println("-- funPc() --");
 	}
 	
-	// servlet 생성
 	@Override
 	public void init() throws ServletException {
-		System.out.println("--- init() ---");
+		System.out.println("-- init() --");
 	}
 	
-	// servlet 종료
 	@Override
 	public void destroy() {
-		System.out.println("--- destroy() ---");
+		System.out.println("-- destroy() --");
 	}
 	
 	@PreDestroy
-	public void preDestory() {
-		System.out.println("--- preDestory() ---");
+	public void funPd() {
+		System.out.println("-- funPd() --");
 	}
+	
 }
