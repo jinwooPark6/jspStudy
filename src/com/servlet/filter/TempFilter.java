6a69197dd89e4dfdcfb318d96ea5c7a894a1314a
jinwooPark6 <jinwoo13245@naver.com>
2021-03-22 19:59:45 +0900

    한글처리 & Filter

---
+++ src/com/servlet/filter/TempFilter.java
@@ -0,0 +1,38 @@
+package com.servlet.filter;
+
+import java.io.IOException;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+
+public class TempFilter implements Filter {
+
+	@Override
+	public void init(FilterConfig fConfig) throws ServletException {
+		System.out.println(" --- filter init() ---");
+		
+	}
+	
+	@Override
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+		System.out.println(" --- filter doFilter() --- ");
+		
+		// request filter
+		request.setCharacterEncoding("UTF-8");
+		
+		chain.doFilter(request, response);
+		
+		// response filter
+		
+	}
+
+	@Override
+	public void destroy() {
+		System.out.println(" --- filter destroy() --- ");
+	}
+
+}
